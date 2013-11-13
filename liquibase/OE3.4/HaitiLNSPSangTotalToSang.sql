fae59d8e8bfe515404a0633f5d4081b6dcccb6cf
Paul Schwartz <paulsc@uw.edu>
2013-11-13 11:03:43 -0800

    HaitiLNSP move tests and panels from Sang Total to Sang
    https://www.pivotaltracker.com/story/show/60636978

---
+++ liquibase/OE3.4/HaitiLNSPSangTotalToSang.sql
@@ -0,0 +1,5 @@
+update clinlims.test set description=replace(description, ' Total', '') where description like '%Sang Total%';
+update clinlims.sampletype_test set sample_type_id=(select id from clinlims.type_of_sample where description='Sang') where sample_type_id=(select id from clinlims.type_of_sample where description='Sang Total');
+update clinlims.sampletype_panel set sample_type_id=(select id from clinlims.type_of_sample where description='Sang') where sample_type_id=(select id from clinlims.type_of_sample where description='Sang Total');
+update clinlims.sample_item set typeosamp_id=(select id from clinlims.type_of_sample where description='Sang') where typeosamp_id=(select id from clinlims.type_of_sample where description='Sang Total');
+update clinlims.type_of_sample set is_active='N' where description='Sang Total';
