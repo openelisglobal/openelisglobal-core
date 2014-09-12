fbb175c4152f0ae3e72f9f2f704c362285833d12
jfurlong <jfurlong@uw.edu>
2014-09-12 11:27:37 -0400

    Remove results so that prior commit runs.  Sanity never came my way....

--- liquibase/OE3.4/testCatalogHT_LNSP/testUpdates15.sql
+++ liquibase/OE3.4/testCatalogHT_LNSP/testUpdates15.sql
@@ -1,10 +1,18 @@
+delete from clinlims.result_signature where result_id in (select id from clinlims.result where test_result_id in (select id from clinlims.test_result where test_id in (select id from clinlims.test where description = 'Score d''adherence(DBS)' )));
+delete from clinlims.result_signature where result_id in (select id from clinlims.result where test_result_id in (select id from clinlims.test_result where test_id in (select id from clinlims.test where description = 'Date de mise sous ARV(DBS)' )));
+
+delete from clinlims.result where test_result_id in (select id from clinlims.test_result where test_id in (select id from clinlims.test where description = 'Score d''adherence(DBS)' ));
+
+delete from clinlims.result where test_result_id in (select id from clinlims.test_result where test_id in (select id from clinlims.test where description = 'Date de mise sous ARV(DBS)' ));
+
 delete from clinlims.test_result where test_id in (select id from clinlims.test where description = 'Score d''adherence(DBS)' );
 
 delete from clinlims.test_result where test_id in (select id from clinlims.test where description = 'Date de mise sous ARV(DBS)' );
 
 
+
 INSERT INTO clinlims.test_result( id, test_id, tst_rslt_type, lastupdated)
-VALUES ( nextval( 'clinlims.test_result_seq' ) , (select id from clinlims.test where description = 'Score d''adherence(DBS)' ) ,
+VALUES ( nextval( 'clinlims.test_result_seq' ) , (select id from clinlims.test where description like 'Score d_adherence(DBS)' ) ,
          'R' ,  now() ) ;
 
 INSERT INTO clinlims.test_result( id, test_id, tst_rslt_type, lastupdated)
