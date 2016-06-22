7cf7f9a3fc68f31e3721d497a2d671ff8a9e4f4b
rossumg <rossumg@uw.edu>
2016-06-22 14:31:17 -0700

    pivotal #121687209

--- liquibase/OE5.3/sampleTypeLocalizationFiles/ciRegionalsampleTypeLocalization.sql
+++ liquibase/OE5.3/sampleTypeLocalizationFiles/ciRegionalsampleTypeLocalization.sql
@@ -8,8 +8,8 @@ INSERT INTO localization(  id, description, english, french, lastupdated)
 VALUES(nextval('localization_seq'), 'sampleType name', 'Sang total', 'Sang total', now());
 UPDATE type_of_sample set name_localization_id = currval('localization_seq') where description = 'Sang total';
 INSERT INTO localization(  id, description, english, french, lastupdated)
-VALUES(nextval('localization_seq'), 'sampleType name', 'Sérum', 'Sérum', now());
-UPDATE type_of_sample set name_localization_id = currval('localization_seq') where description like 'Sérum';
+VALUES(nextval('localization_seq'), 'sampleType name', 'Serum', 'Sérum', now());
+UPDATE type_of_sample set name_localization_id = currval('localization_seq') where description like 'Serum';
 INSERT INTO localization(  id, description, english, french, lastupdated)
 VALUES(nextval('localization_seq'), 'sampleType name', 'Urines', 'Urines', now());
 UPDATE type_of_sample set name_localization_id = currval('localization_seq') where description = 'Urines';
