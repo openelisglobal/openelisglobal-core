907204be4f061393a37efdd824d021f820016fe2
phassoa <phassoa@itech-cotedivoire.org>
2016-12-07 21:15:02 +0000

    Revert "Merge pull request #4 from openelisglobal/develop"
    This reverts commit e9b7b0d78f93135f46cb2ba67277d5f8387edc15, reversing
    changes made to 23c8bee4eb4065905601724e6f77fe6850672550.

--- liquibase/OE5.3/sampleTypeLocalizationFiles/ciLNSPsampleTypeLocalization.sql
+++ liquibase/OE5.3/sampleTypeLocalizationFiles/ciLNSPsampleTypeLocalization.sql
@@ -1,5 +1,5 @@
 INSERT INTO localization(  id, description, english, french, lastupdated)
-VALUES(nextval('localization_seq'), 'sampleType name', 'Variable', 'Variï¿½', now());
+VALUES(nextval('localization_seq'), 'sampleType name', 'Variable', 'Varié', now());
 UPDATE type_of_sample set name_localization_id = currval('localization_seq') where description = 'Actual type will be selected by user';
 INSERT INTO localization(  id, description, english, french, lastupdated)
 VALUES(nextval('localization_seq'), 'sampleType name', 'Plasma', 'Plasma', now());
@@ -8,8 +8,8 @@ INSERT INTO localization(  id, description, english, french, lastupdated)
 VALUES(nextval('localization_seq'), 'sampleType name', 'Sang total', 'Sang total', now());
 UPDATE type_of_sample set name_localization_id = currval('localization_seq') where description = 'Sang total';
 INSERT INTO localization(  id, description, english, french, lastupdated)
-VALUES(nextval('localization_seq'), 'sampleType name', 'Serum', 'SÃ©rum', now());
-UPDATE type_of_sample set name_localization_id = currval('localization_seq') where description = 'SÃ©rum';
+VALUES(nextval('localization_seq'), 'sampleType name', 'Sérum', 'Sérum', now());
+UPDATE type_of_sample set name_localization_id = currval('localization_seq') where description = 'Sérum';
 INSERT INTO localization(  id, description, english, french, lastupdated)
 VALUES(nextval('localization_seq'), 'sampleType name', 'Urines', 'Urines', now());
 UPDATE type_of_sample set name_localization_id = currval('localization_seq') where description = 'Urines';
