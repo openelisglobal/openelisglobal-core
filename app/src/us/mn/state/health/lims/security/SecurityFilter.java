e74f62e91a8eca1996183f0599af448e5edb40c8
CalebSLane <caleblane40@gmail.com>
2017-08-17 15:45:20 -0600

    persistent xss security, and security fixes

--- app/src/us/mn/state/health/lims/security/SecurityFilter.java
+++ app/src/us/mn/state/health/lims/security/SecurityFilter.java
@@ -2,6 +2,8 @@ package us.mn.state.health.lims.security;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.HashSet;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -16,8 +18,10 @@ import us.mn.state.health.lims.common.log.LogEvent;
 
 public class SecurityFilter implements Filter {
 
+	HashSet<String> getParamWhiteList;
+	HashSet<String> getParamBlackList;
+	
 	public SecurityFilter() {
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
@@ -29,13 +33,15 @@ public class SecurityFilter implements Filter {
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response,
 			FilterChain chain) throws IOException, ServletException {
+
 		HttpServletRequest httpRequest = (HttpServletRequest) request;
 		HttpServletResponse httpResponse = (HttpServletResponse) response;
 		boolean suspectedAttack = false;
 		ArrayList<String> attackList = new ArrayList<String>();
 		
-		//CSRF check
-		if (httpRequest.getMethod().equals("POST")) {
+		//CSRF check for any "action" pages
+		if (httpRequest.getMethod().equals("POST") || httpRequest.getRequestURI().contains("Update")
+				|| httpRequest.getRequestURI().contains("Save")) {
 			String referer = httpRequest.getHeader("Referer");
 			String scheme = httpRequest.getScheme();
 			String host = httpRequest.getHeader("Host");
@@ -43,28 +49,42 @@ public class SecurityFilter implements Filter {
 			String baseURL = scheme + "://" + host + contextPath;			
 			if  (referer == null) {
 				suspectedAttack = true;
-				attackList.add("CSRF");
+				attackList.add("CSRF- null referer");
 			} else if (!referer.startsWith(baseURL)) {
 				suspectedAttack = true;
-				attackList.add("CSRF");
+				attackList.add("CSRF- " + referer);
 			} 
 		}
-		
-		//Body Parameters in query check
-		String[] blacklist = {"loginName=", "password=", "selectedIDs=", "patientProperties.", "sampleOrderItems.",
-				"selectedRoles=", "account", "expirationDate=0", "qaEvents"};
+			
+		//Body Parameters in query check currently on blacklist
 		String query = httpRequest.getQueryString();
 		if (query != null) {
 			String[] urlParams = query.split("&");
 			for (int i = 0; i < urlParams.length; i++) {
-				String urlParam = urlParams[i];
-				for (int j = 0; j < blacklist.length; j++) {
-					if (urlParam.startsWith(blacklist[j])) {
-						suspectedAttack = true;
-						attackList.add("Body Parameter in query");
-					}
+				String urlParamName = urlParams[i].split("=")[0];
+				if (urlParamName.contains(".")) {
+					urlParamName = urlParamName.split(".")[0];
+				}
+				//if (!getParamWhiteList.contains(urlParamName)) {
+				if (getParamBlackList.contains(urlParamName)) {
+					suspectedAttack = true;
+					attackList.add("Body Parameter in query- " + urlParamName);
 				}
 			}
+		}
+			
+		//XSS check 
+		if (httpRequest.getMethod().equals("POST")) {
+			Enumeration<String> parameterNames = httpRequest.getParameterNames();
+			 while (parameterNames.hasMoreElements()) {
+				 String param = httpRequest.getParameter(parameterNames.nextElement());
+				 String paramValue = java.net.URLDecoder.decode(param, "UTF-8");
+				 paramValue = paramValue.replaceAll("\\s", "");
+				 if (paramValue.contains("<script>") || paramValue.contains("</script>")) {
+					 suspectedAttack = true;
+					 attackList.add("XSS- " + param);
+				 }
+			 }
 		}
 		
 		//Adding security headers to response
@@ -74,17 +94,23 @@ public class SecurityFilter implements Filter {
 		httpResponse.addHeader("X-Content-Type-Options","nosniff"); //prevents MIME sniffing errors
 		httpResponse.addHeader("X-Frame-Options", "SAMEORIGIN");//enforces whether page is allowed to be an iframe in another website
 		httpResponse.addHeader("X-XSS-Protection","1"); //provides browser xss protection. attempts to cleanse.
-		
+	
 		if (!suspectedAttack) {
-			chain.doFilter(httpRequest, httpResponse);
+			chain.doFilter(request, httpResponse);
 		} else {
-			StringBuilder attacks = new StringBuilder();
+			StringBuilder attackMessage = new StringBuilder();
+			String separator = "";
+			attackMessage.append(httpRequest.getRequestURI());
+			attackMessage.append(" suspected attack(s) of type: ");
 			for (String attack : attackList) {
-				attacks.append(attack);
-				attacks.append("\t");
+				attackMessage.append(separator);
+				separator = ",";
+				attackMessage.append(attack);
 			}
+			
 			//should log suspected attempt
-			LogEvent.logWarn("SecurityFilter", "doFilter()", "Suspected Attack of type:" + attacks.toString());
+			LogEvent.logWarn("SecurityFilter", "doFilter()", attackMessage.toString());
+			System.out.println(attackMessage.toString());
 			//send to safe page
 			httpResponse.sendRedirect("Dashboard.do");
 		}
@@ -92,8 +118,62 @@ public class SecurityFilter implements Filter {
 
 	@Override
 	public void init(FilterConfig filterConfig) throws ServletException {
-		// TODO Auto-generated method stub
+		getParamWhiteList = new HashSet<String>();
+		//createWhiteList();
+		getParamBlackList = new HashSet<String>();
+		createBlackList();
+	}
+	
+	//less restrictive, less likely to stop valid traffic
+	private void createBlackList() {
+		getParamBlackList.add("englishValue");
+		getParamBlackList.add("frenchValue");
+		getParamBlackList.add("loginName");
+		getParamBlackList.add("password");
+		getParamBlackList.add("patientProperties");
+		getParamBlackList.add("qaEvents");
+		getParamBlackList.add("sampleOrderItems");
+		getParamBlackList.add("selectedIDs");
+		getParamBlackList.add("selectedRoles");
+	}
 
+	//more restrictive, more likely to stop valid traffic
+	public void createWhiteList() {
+		getParamWhiteList.add("accessionNumber");
+		getParamWhiteList.add("accessionNumberSearch");
+		getParamWhiteList.add("blank");
+		getParamWhiteList.add("cacheBreaker");
+		getParamWhiteList.add("date");
+		getParamWhiteList.add("field");
+		getParamWhiteList.add("fieldId");
+		getParamWhiteList.add("firstName");
+		getParamWhiteList.add("forward");
+		getParamWhiteList.add("guid");
+		getParamWhiteList.add("ID");
+		getParamWhiteList.add("labNo");
+		getParamWhiteList.add("labNumber");
+		getParamWhiteList.add("lang");
+		getParamWhiteList.add("lastName");
+		getParamWhiteList.add("NationalID");
+		getParamWhiteList.add("nationalID");
+		getParamWhiteList.add("patientID");
+		getParamWhiteList.add("personKey");
+		getParamWhiteList.add("provider");
+		getParamWhiteList.add("regionId");
+		getParamWhiteList.add("relativeToNow");
+		getParamWhiteList.add("report");
+		getParamWhiteList.add("sampleType");
+		getParamWhiteList.add("selectedSearchID");
+		getParamWhiteList.add("selectedValue");
+		getParamWhiteList.add("startingRecNo");
+		getParamWhiteList.add("STNumber");
+		getParamWhiteList.add("subjectNumber");
+		getParamWhiteList.add("suppressExternalSearch");
+		getParamWhiteList.add("test");
+		getParamWhiteList.add("testSectionId");
+		getParamWhiteList.add("type");
+		getParamWhiteList.add("value");
+		getParamWhiteList.add("ver");
 	}
 
 }
