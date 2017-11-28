78aa0f478c0ecae0b0bb098ff71db2f7ee77d884
CalebSLane <caleblane40@gmail.com>
2017-11-28 15:49:52 -0700

    database changes for barcodes and batch entry

--- app/src/us/mn/state/health/lims/common/util/DefaultConfigurationProperties.java
+++ app/src/us/mn/state/health/lims/common/util/DefaultConfigurationProperties.java
@@ -116,8 +116,8 @@ public class DefaultConfigurationProperties extends ConfigurationProperties {
 		setDBPropertyMappingAndDefault( Property.MAX_ORDER_PRINTED, "numOrderLabels", "10");
 		setDBPropertyMappingAndDefault( Property.MAX_SPECIMEN_PRINTED, "numSpecimenLabels", "1");
 		setDBPropertyMappingAndDefault( Property.MAX_ALIQUOT_PRINTED, "numAliquotLabels", "1");
-		setDBPropertyMappingAndDefault( Property.ORDER_BARCODE_HEIGHT, "heightOrderLabels", "1");
-		setDBPropertyMappingAndDefault( Property.ORDER_BARCODE_WIDTH, "widthOrderLabels", "3");
+		setDBPropertyMappingAndDefault( Property.ORDER_BARCODE_HEIGHT, "heightOrderLabels", "25.6");
+		setDBPropertyMappingAndDefault( Property.ORDER_BARCODE_WIDTH, "widthOrderLabels", "76.2");
 		setDBPropertyMappingAndDefault( Property.SPECIMEN_BARCODE_HEIGHT, "heightSpecimenLabels", "25.6");
 		setDBPropertyMappingAndDefault( Property.SPECIMEN_BARCODE_WIDTH, "widthSpecimenLabels", "76.2");
 		setDBPropertyMappingAndDefault( Property.SPECIMEN_FIELD_DATE, "collectionDateCheck", "true");
